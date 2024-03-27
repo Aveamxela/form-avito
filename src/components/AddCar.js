7d311272848c86938dff024327ca0e9c355852c2
Stella Ndjikessi <stelladiana@live.fr>
2024-03-27 12:28:29 +0100

    Component Label créé

--- src/components/AddCar.js
+++ src/components/AddCar.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import Select from "./Select";
-import Input from "./components/Input";
-import Textarea from "./components/Textarea";
+import Input from "./Input";
+import Textarea from "./Textarea";
+import Label from "./Label";
 
 const AddCar = () => {
   const [formData, setFormData] = useState({
@@ -26,21 +27,25 @@ const AddCar = () => {
         <div>
           <Input />
           <Textarea />
+          <Label text="Marque" />
           <Select
             name="marque"
             options={['','MG', 'Alpha Romeo', 'Chevrolet', 'Chrysler']}
             value={formData.marque}
             onChange={handleChange}/>
+          <Label text="Année-Modèle" />
           <Select
             name="annee"
             options={['', 2024, 2023, 2022, 2021]}
             value={formData.annee}
             onChange={handleChange} />
+          <Label text="Kilométrage" />
           <Select
             name="kilometrage"
             options={['', '0 - 4 999', '5 000 - 9 999', '10 000 - 14 999', '15 000 - 19 999']}
             value={formData.kilometrage}
             onChange={handleChange} />
+          <Label text="Puissance fiscale" />
           <Select
             name="puissanceFiscale"
             options={['', '4C', '5CV', '6CV', '7CV']}
