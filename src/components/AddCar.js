900bd15a0facf67d4df06d0d8e43a189b9fd3cf5
Maeva <maeva-froger@outlook.fr>
2024-03-27 17:28:26 +0100

    stateRadioBtn_OK

--- src/components/AddCar.js
+++ src/components/AddCar.js
@@ -5,84 +5,116 @@ import Textarea from "./Textarea";
 import Label from "./Label";
 import RadioBtns from "./RadioBtns";
 
-
 const AddCar = ({ options }) => {
-  const { marque, anneeModele, kilometrage, puissanceFiscale, fake: { faker } } = options;
-  console.log(faker);
+    const {
+        marque,
+        anneeModele,
+        kilometrage,
+        puissanceFiscale,
+        fake: { faker },
+    } = options;
+    console.log(faker);
+    const [globalState, setGlobalState] = useState({ radio: "" });
+    const [formData, setFormData] = useState({
+        marque: "",
+        annee: "",
+        kilometrage: 0,
+        puissanceFiscale: "",
+        prix: 0,
+        titre: "",
+    });
 
-  const [formData, setFormData] = useState({
-      marque: "",
-      annee: "",
-      kilometrage: 0,
-      puissanceFiscale: "",
-      prix: 0,
-      titre: "",
-      "Type de carburant": "",
-      "Boite de vitesses": "",
-      Etat: "",
-      Origine: "",
-      "Nombre de portes": "",
-      "Première main": "",
-  });
+    const handleChange = (e) => {
+        const { name, value } = e.target;
 
-  const handleChange = e => {
-    const { name, value } = e.target
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
 
-    setFormData(prevState => ({
-      ...prevState,
-      [name]: value
-    }))
-  }
+    const handleChangeRadio = (radioChoice) => {
+        setGlobalState({ radio: radioChoice });
+    };
 
-  return (
-    <>
-      <form>
-        <div>
-          <Label text="Marque" />
-          <Select
-            name="marque"
-            options={marque}
-            value={formData.marque}
-            onChange={handleChange}/>
-          <Label text="Année-Modèle" />
-          <Select
-            name="annee"
-            options={anneeModele}
-            value={formData.annee}
-            onChange={handleChange} />
-          <Label text="Kilométrage" />
-          <Select
-            name="kilometrage"
-            options={kilometrage}
-            value={formData.kilometrage}
-            onChange={handleChange} />
-          <Label text="Puissance fiscale" />
-          <Select
-            name="puissanceFiscale"
-            options={puissanceFiscale}
-            value={formData.puissanceFiscale}
-            onChange={handleChange} />
-        </div>
+    return (
+        <>
+            <form>
+                <div>
+                    <Label text="Marque" />
+                    <Select
+                        name="marque"
+                        options={marque}
+                        value={formData.marque}
+                        onChange={handleChange}
+                    />
+                    <Label text="Année-Modèle" />
+                    <Select
+                        name="annee"
+                        options={anneeModele}
+                        value={formData.annee}
+                        onChange={handleChange}
+                    />
+                    <Label text="Kilométrage" />
+                    <Select
+                        name="kilometrage"
+                        options={kilometrage}
+                        value={formData.kilometrage}
+                        onChange={handleChange}
+                    />
+                    <Label text="Puissance fiscale" />
+                    <Select
+                        name="puissanceFiscale"
+                        options={puissanceFiscale}
+                        value={formData.puissanceFiscale}
+                        onChange={handleChange}
+                    />
+                </div>
 
-        <div>
-          <Label text="Prix" />
-          <Input
-            name="prix"
-            value={formData.prix}
-            type="number"
-            onChange={handleChange} />
-          <Label text="Texte de l'annonce"/>
-          <Input
-            name="titre"
-            value={formData.titre}
-            type="text"
-            onChange={handleChange} />
-        </div>
-        <RadioBtns/>
-        <Textarea />
-      </form>
-    </>
-  )
-}
+                <div>
+                    <Label text="Prix" />
+                    <Input
+                        name="prix"
+                        value={formData.prix}
+                        type="number"
+                        onChange={handleChange}
+                    />
+                    <Label text="Texte de l'annonce" />
+                    <Input
+                        name="titre"
+                        value={formData.titre}
+                        type="text"
+                        onChange={handleChange}
+                    />
+                </div>
+                <RadioBtns
+                    title="Type de carburant"
+                    labels={[
+                        "Diesel",
+                        "Essence",
+                        "Electrique",
+                        "LPG",
+                        "Hybride",
+                    ]}
+                    name="type_carburant"
+                    onChoiceradio={handleChangeRadio}
+                />
+                <RadioBtns
+                    title="Type de carburant"
+                    labels={[
+                        "Diesel",
+                        "Essence",
+                        "Electrique",
+                        "LPG",
+                        "Hybride",
+                    ]}
+                    name="type_carburant"
+                    onChoiceradio={handleChangeRadio}
+                />
+                <Textarea />
+            </form>
+        </>
+    );
+};
 
 export default AddCar;
--- src/components/RadioBtns.js
+++ src/components/RadioBtns.js
@@ -1,78 +1,22 @@
-import { useState } from "react";
-
-const RadioBtns = (onChange) => {
-    const radioForm = [
-        {
-            name: "Type de carburant",
-            options: ["Diesel", "Essence", "Electrique", "LPG", "Hybride"],
-        },
-        { name: "Boite de vitesses", options: ["Automatique", "Manuelle"] },
-        {
-            name: "Etat",
-            options: [
-                "Excellent",
-                "Très bon",
-                "Bon",
-                "Correct",
-                "Endommagé",
-                "Pour Pièces",
-                "Neuf",
-            ],
-        },
-        {
-            name: "Origine",
-            options: [
-                "Dédouanée",
-                "Pas encore dédouanée",
-                "WW au Maroc",
-                "Importée neuve",
-            ],
-        },
-        { name: "Nombre de portes", options: ["3", "5"] },
-        { name: "Première main", options: ["Oui", "Non"] },
-    ];
-
-    const [selectedOption, setSelectedOption] = useState({
-        "Type de carburant": "",
-        "Boite de vitesses": "",
-        "Etat": "",
-        "Origine": "",
-        "Nombre de portes": "",
-        "Première main": "",
-    });
-    console.log({selectedOption})
+const RadioBtns = ({ title, labels, name, onChoiceradio }) => {
     
-    const handleChangeRadio = (event) => {
-        const { name, value} = event.target;
-        console.log(name)
-        setSelectedOption((prevState) => {
-            return {
-                ...prevState,
-                [name] : value
-            }
-            
-        })
-    };
-
-    const listRadioBtns = radioForm.map((item, index) => (
-        <div key={index}>
-            <p>{item.name}</p>
-            {/* Mapper sur les options et créer input avec :  */}
-            {item.options.map((option, optionIndex) => (
-                <div key={optionIndex}>
-                    <input
-                        type="radio"
-                        name={item.name}
-                        value={option}
-                        //Vérifie que la valeur contenue dans notre catégorie est égale à l'option
-                        checked={selectedOption[item.name]===option}
-                        onChange={handleChangeRadio}
-                    />
-                    <label>{option}</label>
-                </div>
-            ))}
-        </div>
-    ));
-    return <div>{listRadioBtns}</div>;
+    return (
+        <>
+            <h5>{title}</h5>
+            {labels.map((label, index, e) => {
+                return (
+                    <div key={index}>
+                        <label>{label}</label>
+                        <input
+                            type="radio"
+                            name={name}
+                            value={label}
+                            onChange={(e) => onChoiceradio(e.target.value)}
+                        />
+                    </div>
+                );
+            })}
+        </>
+    );
 };
 export default RadioBtns;
