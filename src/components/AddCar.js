5ec4e84258a23e03922c7a52e4e777cd339c5193
Stella Ndjikessi <stelladiana@live.fr>
2024-03-27 12:05:56 +0100

    Les Select pour le km et puissance ont été ajouté

--- src/components/AddCar.js
+++ src/components/AddCar.js
@@ -22,8 +22,26 @@ const AddCar = () => {
     <>
       <form>
         <div>
-          <Select name="marque" options={['','MG', 'Alpha Romeo', 'Chevrolet', 'Chrysler']} value={formData.marque} onChange={handleChange}/>
-          <Select name="annee" options={['', 2024, 2023, 2022, 2021]} value={formData.annee} onChange={handleChange} />
+          <Select
+            name="marque"
+            options={['','MG', 'Alpha Romeo', 'Chevrolet', 'Chrysler']}
+            value={formData.marque}
+            onChange={handleChange}/>
+          <Select
+            name="annee"
+            options={['', 2024, 2023, 2022, 2021]}
+            value={formData.annee}
+            onChange={handleChange} />
+          <Select
+            name="kilometrage"
+            options={['', '0 - 4 999', '5 000 - 9 999', '10 000 - 14 999', '15 000 - 19 999']}
+            value={formData.kilometrage}
+            onChange={handleChange} />
+          <Select
+            name="puissanceFiscale"
+            options={['', '4C', '5CV', '6CV', '7CV']}
+            value={formData.puissanceFiscale}
+            onChange={handleChange} />
         </div>
       </form>
     </>
